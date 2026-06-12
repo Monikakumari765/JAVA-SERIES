2ed534ac7994ef7a570d78c44827a95f057df4fd
Monika kumari <monikakumari4687@gmail.com>
2026-06-12 11:22:28 +0530

    practice

---
+++ NestedIfExample.java
@@ -0,0 +1,16 @@
+public class NestedIfExample {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean hasID = true;
+
+        if (age >= 18) {
+            if (hasID) {
+                System.out.println("You are eligible to vote.");
+            } else {
+                System.out.println("Please bring your ID card.");
+            }
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
