04b711af314a71c7c1e4c5baadab36969f93e17c
Monika kumari <monikakumari4687@gmail.com>
2026-06-12 11:24:06 +0530

    practice

---
+++ ifexample.java
@@ -0,0 +1,13 @@
+public class ifexample {
+   public ifexample() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 5;
+      byte var2 = 4;
+      if (var1 > var2) {
+         System.out.println("a is greater than b");
+      }
+
+   }
+}
